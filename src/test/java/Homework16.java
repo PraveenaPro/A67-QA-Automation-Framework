6c21de072831ce16f6207ed566913307103f446a
PraveenaPro <praveena.chitramanalan@testpro.io>
2025-02-06 18:04:59 -0700

    Corrected build errors

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -11,9 +11,12 @@ import java.time.Duration;
 public class Homework16 extends BaseTest {
     @Test
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     public void registrationNavigation(){
         ChromeOptions options = new ChromeOptions();
 =======
+=======
+>>>>>>> Stashed changes
 
     void registrationNavigation() {
         Chromeoptions options = new Chromeoptions();
@@ -27,15 +30,21 @@ public class Homework16 extends BaseTest {
         driver.get(url);
         WebElement registrationLink = driver.findElement(By.cssSelector("[href='registration']"));
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         registrationLink.click();
 
         String registrationurl = "https://qa.koal.app/registration";
         Assert.assertEquals(driver.getCurrentUrl(),registrationurl);
 =======
+=======
+>>>>>>> Stashed changes
         registration.click();
 
         String registrationurl = "https://qa.koal.app/registration";
         Assert.arrertEquals(driver.getCurrentUrl(), registrationurl);
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 
         driver.quit();
