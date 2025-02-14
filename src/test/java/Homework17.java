64540cc023a008362d6e50e871ef04c9b3d234a3
PraveenaPro <praveena.chitramanalan@testpro.io>
2025-02-13 20:44:40 -0700

    Create Homework17.java

---
+++ src/test/java/Homework17.java
@@ -0,0 +1,32 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework17 extends BaseTest{
+    @Test
+    public void addSongToPlayList() throws InterruptedException{
+
+        String strexpectedSongAddedMessage = "Added 1 song into myplaylist";
+        String strSongName = "Pluto";
+        String strEmail = "praveena.chitramanalan@testpro.io";
+        String strPassword = "TestPro@0315";
+
+        navigateToPage();
+        provideEmail(strEmail);
+        providePassword(strPassword);
+        clickSubmit();
+        Thread.sleep(2000);
+
+        //searchSong(strSongName);
+        clickViewAllBtn();
+        selectFirstSongResult();
+        clickAddToBtn();
+        choosePlayList();
+
+        Assert.assertEquals(getAddToPlayListSuccessMsg(),strexpectedSongAddedMessage);
+
+
+
+
+    }
+
+}
